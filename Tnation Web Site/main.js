75e5501b4f65fbfca43b8d8ada6aba4288734d06
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-06-12 21:51:49 +0200

    tnation

---
+++ Tnation Web Site/main.js
@@ -0,0 +1,41 @@
+function myFunction() {
+    let job = document.getElementById("dropdown-job").children;
+    let associates = document.getElementById("dropdown-associates").children;
+    let contact = document.getElementById("dropdown-contact").children;
+    for (let i = 0; i < job.length; i++) {
+        job[i].id = i;
+        job[i].addEventListener("mouseover", mouseOver);
+        job[i].addEventListener("mouseout", mouseOut);
+    }
+    for (let i = 0, x = 3; i < associates.length; i++) {
+        associates[i].id = x;
+        associates[i].addEventListener("mouseover", mouseOver);
+        associates[i].addEventListener("mouseout", mouseOut);
+        x++;
+    }
+    for (let i = 0, x = 6; i < contact.length; i++) {
+        contact[i].id = x;
+        contact[i].addEventListener("mouseover", mouseOver);
+        contact[i].addEventListener("mouseout", mouseOut);
+        x++;
+    }
+}
+myFunction();
+
+
+function mouseOver() {
+    if (this.id == 0 || this.id == 4) {
+        this.style.backgroundColor = "#CC88EE";
+
+    }
+    if (this.id == 1 || this.id == 5 || this.id == 6) {
+        this.style.backgroundColor = "#FF9999";
+    };
+    if (this.id == 2 || this.id == 3 || this.id == 7) {
+        this.style.backgroundColor = "#FFFF99";
+    }
+}
+
+function mouseOut() {
+    this.style.backgroundColor = "rgba(36,36,255,0.5)";
+}
